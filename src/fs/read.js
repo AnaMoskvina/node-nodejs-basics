f5297515d9c24f1c6de467da86e06b158ed5aa7c
Anastasiia Moskvina <anastasiia_moskvina@epam.com>
2022-06-18 15:31:50 +0300

    add fs/read solution

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,30 @@
-export const read = async () => {
-    // Write your code here 
-};
+// implement function that prints content 
+// of the fileToRead.txt into console (if there's no file fileToRead.txt 
+// Error with message FS operation failed must be thrown)
+
+import fs, { read } from 'fs'
+import fsPromises from 'fs/promises'
+
+const errorMessage = 'FS operation failed'
+const filePath = './files/fileToRead.txt'
+
+// 1. async
+// export const read = () => {
+//     fs.readFile(filePath, 'utf8', (err, data) => {
+//         if (err) throw new Error(errorMessage)
+//         console.log(data)
+//     })
+// }
+
+// read()
+
+// 2. promises
+export const read2 = async () => {
+    try {
+        console.log(await fsPromises.readFile(filePath, 'utf8'))
+    } catch (_err) {
+        throw new Error(errorMessage)
+    }
+}
+
+read2()
