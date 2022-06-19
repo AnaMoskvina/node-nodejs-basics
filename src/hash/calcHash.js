56739195e4bc5f2a4c5bb0fa2fb22c5bdfb8432c
Anastasiia Moskvina <anastasiiamoskvina@Anastasiias-MacBook-Pro.local>
2022-06-19 14:54:26 +0300

    add modules solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,6 @@
+// implement function that calculates SHA256 hash 
+// for file fileToCalculateHashFor.txt and return it as hex
+
 export const calculateHash = async () => {
     // Write your code here 
 };
