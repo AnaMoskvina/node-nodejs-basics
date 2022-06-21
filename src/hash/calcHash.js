c8c52f60bcaa8b69061615d240a8381fc87fc858
Anastasiia Moskvina <anastasiiamoskvina@Anastasiias-MacBook-Pro.local>
2022-06-21 18:17:57 +0300

    add hash task solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,6 +1,33 @@
 // implement function that calculates SHA256 hash 
 // for file fileToCalculateHashFor.txt and return it as hex
 
-export const calculateHash = async () => {
-    // Write your code here 
-};
+import fs from 'fs'
+import { stdout } from 'process'
+const {
+    createHash
+  } = await import('node:crypto')
+
+const filePath = './files/fileToCalculateHashFor.txt'
+
+// 1. with streams
+export const calculateHash = () => {
+  const hash = createHash('sha256')
+  fs.createReadStream(filePath)
+    .pipe(hash)
+    .setEncoding('hex')
+    .pipe(stdout)
+}
+
+calculateHash()
+
+// 2. with update and digest methods
+// export const calculateHash1 = () => {
+//     const hash = createHash('sha256')
+//     fs.readFile(filePath, (err, data) => {
+//       if (err) throw err
+//       hash.update(data)
+//       return hash.digest('hex')
+//     })   
+// }
+
+// calculateHash1()
