ab9ef24bcd926b530abe0763ab9abf2a8261a8ca
Anastasiia Moskvina <anastasiiamoskvina@Anastasiias-MacBook-Pro.local>
2022-06-22 00:04:12 +0300

    add zip tasks solution

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,38 @@
-export const compress = async () => {
-    // Write your code here 
-};
+// implement function that compresses file fileToCompress.txt 
+// to archive.gz using zlib and Streams API
+
+import { createGzip } from 'zlib'
+import { pipeline } from 'stream'
+import { createReadStream, createWriteStream } from 'fs'
+import { promisify } from 'util'
+
+const pathToSource = './files/fileToCompress.txt'
+const pathToDestination = './files/archive.gz'
+
+// 1. async
+// export const compress = () => {
+//     const gzip = createGzip()
+//     const source = createReadStream(pathToSource)
+//     const destination = createWriteStream(pathToDestination)
+
+//     pipeline(source, gzip, destination, err => {
+//         if (err) process.exitCode = 1
+//     })
+// }
+
+// compress()
+
+// 2. promisees
+export const compress1 = async () => {
+    try {
+        const gzip = createGzip()
+        const pipe = promisify(pipeline)
+        const source = createReadStream(pathToSource)
+        const destination = createWriteStream(pathToDestination)
+        await pipe(source, gzip, destination)
+    } catch (err) {
+        process.exitCode = 1
+    }
+}
+
+compress1()
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,39 @@
-export const decompress = async () => {
-    // Write your code here 
-};
+// implement function that decompresses archive.gz 
+// back to the fileToCompress.txt with same content 
+// as before compression using zlib and Streams API
+
+import { createUnzip } from 'zlib'
+import { pipeline } from 'stream'
+import { createReadStream, createWriteStream } from 'fs'
+import { promisify } from 'util'
+
+const pathToArchive = './files/archive.gz'
+const pathToDestination = './files/fileToCompress.txt'
+
+// 1. async
+// export const decompress = () => {
+//     const unzip = createUnzip()
+//     const source = createReadStream(pathToArchive)
+//     const destination = createWriteStream(pathToDestination)
+
+//     pipeline(source, unzip, destination, err => {
+//         if (err) process.exitCode = 1
+//     })
+// }
+
+// decompress()
+
+// 2. promises
+export const decompress1 = async () => {
+    try {
+        const unzip = createUnzip()
+        const pipe = promisify(pipeline)
+        const source = createReadStream(pathToArchive)
+        const destination = createWriteStream(pathToDestination)
+        await pipe(source, unzip, destination)
+    } catch(err) {
+        process.exitCode = 1
+    }
+}
+
+decompress1()
