dc27a017e1782ab26e28cb817b790c50697b7cbb
Anastasiia Moskvina <moskvinaanastasia@gmail.com>
2024-01-25 18:45:39 +0400

    refactor transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -6,15 +6,16 @@ import { stdin, stdout } from 'process';
 import { Transform } from 'stream';
 
 const transform = async () => {
-    const transformStream = new Transform();
-    transformStream._transform = (chunk, _encoding, callback) => {
+    const transform = (chunk, _encoding, callback) => {
         const string = chunk.toString();
         // handling line break
-        const firstSymbol = string[string.length - 1];
+        const firstSymbol = string.slice(-1);
         const restSymbols = string.slice(0, -1);
         transformStream.push(restSymbols.split('').reverse().join('') + firstSymbol);
         callback();
-    }
+    };
+
+    const transformStream = new Transform({transform});
     stdin.pipe(transformStream).pipe(stdout);
 };
 
